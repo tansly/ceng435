bc3dc8b287aded1897cf43eb251e028ad1399837
dorukcoskun <1dorukcoskun@gmail.com>
2018-11-23 20:04:26 +0300

    First packet loop implementation

--- source.py
+++ source.py
@@ -1,17 +1,30 @@
 from socket import *
+import datetime
 
-#IP of Broker.
-brokerServerName = '10.10.1.2'
+#IP
+serverName = '10.10.1.2'
+serverPort = 26298 + 300
 
-#Port of Broker.
-brokerServerPort = 26298 + 300
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
 
-#Initialize brokerSocket and connect to Broker.
-brokerSocket = socket(AF_INET, SOCK_STREAM)
-brokerSocket.connect((brokerServerName, brokerServerPort))
+sentTimeList = []
+returnTimeList = []
 
-#Loop
-index = 1
-sentence = chr(index) + 124 * 'a'
-clientSocket.send(sentence.encode())
+for index in range(0, 9):
+    sentence = str(index) + 127 * 'a'
+    clientSocket.send(sentence.encode())
+    sentTimeList.append(datetime.datetime.now().time())
+
+flag = True;
+
+
+while flag:
+    (dataFBroker, addrBroker) = clientSocket.recvfrom(128)
+    returnTimeList.append(datetime.datetime.now().time() - sentTimeList[dataFBroker.decode()[0]])
+    if (size(returnTimeList) == 10):
+        flag = false;
+
+print(returnTimeList)
 clientSocket.close()
+
