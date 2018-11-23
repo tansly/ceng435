264de9aa6734f9eb94c274de7d44293e98b241cd
dorukcoskun <1dorukcoskun@gmail.com>
2018-11-23 20:03:09 +0300

    Packet loop implementation

--- source.py
+++ source.py
@@ -1,11 +1,30 @@
 from socket import *
-from random import randint
+import datetime
+
+#IP
 serverName = '10.10.1.2'
 serverPort = 26298 + 300
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-index = 1
-sentence = str(index) + 127 * 'a'
-clientSocket.send(sentence.encode())
+
+sentTimeList = []
+returnTimeList = []
+
+for index in range(0, 9):
+    sentence = str(index) + 127 * 'a'
+    clientSocket.send(sentence.encode())
+    sentTimeList.append(datetime.datetime.now().time())
+
+flag = True;
+
+
+while flag:
+    (dataFBroker, addrBroker) = clientSocket.recvfrom(128)
+    returnTimeList.append(datetime.datetime.now().time() - sentTimeList[dataFBroker.decode()[0]])
+    if (size(returnTimeList) == 10):
+        flag = false;
+
+print(returnTimeList)
 clientSocket.close()
 
