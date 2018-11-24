872d847e8a097aa933b08cb9a8e36a20a2795442
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-24 20:14:31 +0300

    Make the message size 8 bytes.
    See the commit message in raspipe for discussion.

--- source.py
+++ source.py
@@ -5,18 +5,19 @@ import string
 #IP
 serverName = '10.10.1.2'
 serverPort = 26298 + 300
+msgSize = 8
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
 i = 0
 while True:
-    sentence = 128 * string.ascii_lowercase[i]
+    sentence = msgSize * string.ascii_lowercase[i]
     i = (i + 1) % len(string.ascii_lowercase)
     clientSocket.send(sentence.encode())
     time_sent = time.perf_counter()
 
-    (dataFBroker, addrBroker) = clientSocket.recvfrom(128)
+    (dataFBroker, addrBroker) = clientSocket.recvfrom(msgSize)
     time_recved = time.perf_counter()
     print(dataFBroker.decode())
     print(time_recved - time_sent)
