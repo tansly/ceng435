c6fd40e1d138c2c5d9f4264ffa554607ba28faf3
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-24 10:20:52 +0300

    Alternate the message

--- source.py
+++ source.py
@@ -9,11 +9,14 @@ serverPort = 26298 + 300
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
+i = 0
 while True:
-    sentence = 128 * 'a'
+    sentence = 128 * string.ascii_lowercase[i]
+    i = (i + 1) % len(string.ascii_lowercase)
     clientSocket.send(sentence.encode())
     time_sent = time.perf_counter()
 
     (dataFBroker, addrBroker) = clientSocket.recvfrom(128)
     time_recved = time.perf_counter()
+    print(dataFBroker.decode())
     print(time_recved - time_sent)
