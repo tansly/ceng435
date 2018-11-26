20a431a65f9288abdbc237e16d1e2be2799987d8
dorukcoskun <1dorukcoskun@gmail.com>
2018-11-26 19:29:24 +0300

    Added Packet Loss Test functionality to source.py

--- source.py
+++ source.py
@@ -1,28 +1,39 @@
 import config
-
 from socket import *
 import time
 import string
+import sys
 
-#IP
+#IP and port of Broker.
 serverName = '10.10.1.2'
 serverPort = 26298 + 300
 
+#Create client socket and open TCP connection to Broker.
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
-i = 0
-while True:
-    clientSocket.send((('%' + str(config.msg_size) + 's') % i).encode())
-    time_sent = time.perf_counter()
+#If source.py is run with parameter 'pl_test' then you can observe the result of the Packet Loss Test.
+#Packet Loss Test sends 1000, 8 byte packages wihtout waiting for any packet to return.
+if (sys.argv[1] == 'pl_test'):
+    for _ in range(1, 1000):
+        msg = ('%' + str(config.msg_size) + 's') % i
+        clientSocket.send(msg.encode())
+        print(msg)
 
-    (dataFBroker, addrBroker) = clientSocket.recvfrom(config.msg_size)
-    time_recved = time.perf_counter()
-
-    print(dataFBroker.decode())
-    print(time_recved - time_sent)
-
-    if i >= 10**config.msg_size:
-        i = 0
-    else:
-        i += 1
+#Otherwise you can see the packet delay results. Each packet is sent right after the previous ones feedback comes.
+else:
+    i = 0
+    while True:
+        clientSocket.send((('%' + str(config.msg_size) + 's') % i).encode())
+        time_sent = time.perf_counter()
+    
+        (dataFBroker, addrBroker) = clientSocket.recvfrom(config.msg_size)
+        time_recved = time.perf_counter()
+    
+        print(dataFBroker.decode())
+        print(time_recved - time_sent)
+    
+        if i >= 10**config.msg_size:
+            i = 0
+        else:
+            i += 1
