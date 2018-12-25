f9340b2c67a1ae902bdebf5d664c0dec68e0e0ec
dorukcoskun <1dorukcoskun@gmail.com>
2018-12-25 18:19:34 +0300

    Need Fix multithreaded socket with Lock

--- part2/dest.py
+++ part2/dest.py
@@ -1,29 +1,54 @@
 import config
 
 from socket import*
+import threading
 
 #Port of this server.
 localServerPort = 26299 + 300
 
-#IP of router2
-routerServerName = '10.10.4.2'
+#IP of first interface of Destination (eth1)
+eth1InterfaceIP = '10.10.5.2'
 
-#Port of router2.
-routerServerPort = 26301 + 300
+#IP of first interface of Destination (eth2)
+eth2InterfaceIP = '10.10.3.2'
 
-#Initialize localSocket and listen to localServerPort.
-localSocket = socket(AF_INET, SOCK_DGRAM)
-localSocket.bind(('10.10.3.2', localServerPort))
+seq = 0
+seq_lock = threading.Lock()
 
-#Initialize routerSocket and connect to router2.
-routerSocket = socket(AF_INET, SOCK_DGRAM)
-routerSocket.connect((routerServerName, routerServerPort))
+class ClientThread(threading.Thread):
+    def __init__(self, localIP, localPort):
+        threading.Thread.__init__(self)
+        self.csocket = socket(AF_INET, SOCK_DGRAM)
+        self.csocket.bind((localIP, localPort))
+        print ("New socket thread binded to: ", localIP, ' ', localPort)
+    def run(self):
+        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
+        message = ''
+        while True:
+            (dataReceived, senderAddr) = self.csocket.recvfrom(1000)
+            message = dataReceived.decode()
+            seq_lock.acquire()
+            #Some fuckery
+            if (message[:4])
+                
+            print ("Message received: ", message)
+            ACKMessage = "Selam"
+            self.csocket.sendto(bytes(ACKMessage), senderAddr)
 
-while True:
-    (dataFRouter, addrRouter) = localSocket.recvfrom(config.msg_size)
-    print(dataFRouter.decode())
-    #Sent feedback message.
-    routerSocket.send(dataFRouter)
+eth1SocketThread = ClientThread(eth1InterfaceIP, localServerPort);
+eth2SocketThread = ClientThread(eth2InterfaceIP, localServerPort);
 
-localSocket.close()
-routerSocket.close()
+eth1SocketThread.start();
+eth2SocketThread.start();
+
+eth1SocketThread.join();
+eth2SocketThread.join();
+
+print("Server started")
+print("Waiting for messages")
+
+
+
+
+
+
