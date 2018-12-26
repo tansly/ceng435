f877dd93659b89b55008634a21543cb193eb1d2f
dorukcoskun <1dorukcoskun@gmail.com>
2018-12-26 14:59:02 +0300

    Commented dest.py

--- part2/dest.py
+++ part2/dest.py
@@ -16,10 +16,15 @@ eth2InterfaceIP = '10.10.3.2'
 #Name of the file where the packets received in correct order are written to.
 filename = 'payload'
 
+#SEQ number couter. It is used to ensure the packets are received in the right order. Unexpected packets are discarded.
 seq = 0
+
+#SEQ Lock is used to prevent different socket threads from accesing the SEQ number at the same time.
 seq_lock = threading.Lock()
 
+# Socket thread class.
 class ClientThread(threading.Thread):
+    # Initialize the socket thread by opening a UDP socket and binding it to an Interface.
     def __init__(self, localIP, localPort):
         threading.Thread.__init__(self)
         self.csocket = socket(AF_INET, SOCK_DGRAM)
@@ -28,26 +33,45 @@ class ClientThread(threading.Thread):
     def run(self):
         message = ''
         while True:
-            (dataReceived, senderAddr) = self.csocket.recvfrom(1000)
+            # UDP socket awaits packets.
+            (dataReceived, senderAddr) = self.csocket.recvfrom(config.msg_size)
+            # UDP socket unpacks the received packet.
             (seq_received, checksum, payload) = struct.unpack('!I16s' + str(len(dataReceived) - 20) + 's', dataReceived)
             
+            # SEQ Lock acquired to prevent other treads accesing at the same time.
             seq_lock.acquire()
             global seq
+            
+            # If the packet has the expected SEQ number..
             if (seq_received == seq + 1):
                 seq = seq_received
-                with open(filename, 'ab') as file:
-                    file.write(payload);
+                
+                # If the packets payload is NULL then the file is fully received. Broker handles this.
+                if (len(dataReceived) == 0):
+                    print('File received')
+                
+                # Else write the payload to the file.
+                else:
+                    with open(filename, 'ab') as file:
+                        file.write(payload);
+        
+            # Destination sends an ACK message indicating the SEQ number of the last received expected packet.
             self.csocket.sendto(struct.pack('!I16s', seq, checksum), senderAddr)
+            
+            # SEQ Lock is released after use.
             seq_lock.release()
 
+# Create threads.
 eth1SocketThread = ClientThread(eth1InterfaceIP, localServerPort);
 eth2SocketThread = ClientThread(eth2InterfaceIP, localServerPort);
 
+# Start threads.
 eth1SocketThread.start();
 eth2SocketThread.start();
 
 print("Server started")
 print("Waiting for messages")
 
+# Join the threads so that they would keep running.
 eth1SocketThread.join();
 eth2SocketThread.join();
