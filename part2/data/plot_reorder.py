124a8658aa4869298c4cef38fcc94041b46e4feb
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-12-29 12:40:02 +0300

    Add reorder plot script

---
+++ part2/data/plot_reorder.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from scipy import stats
+import numpy as np
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+plt.ioff()
+
+df = np.loadtxt(sys.argv[1]).T
+
+mean = df.mean(axis = 1)
+std = df.std(axis = 1)
+
+n = df.shape[1]
+yerr = std / np.sqrt(n) * stats.t.ppf(1-0.05/2, n - 1)
+
+x_axis = ['1', '10', '35']
+
+plt.xlabel('Emulated packet reorder (%)')
+plt.ylabel('File transfer time (s)')
+plt.bar(x_axis, mean, yerr = yerr)
+plt.savefig('plot-reorder.png')
